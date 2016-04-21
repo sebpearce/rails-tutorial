14a45fba5283be8077f2a1c8e45a6185be76a598
Seb Pearce <sebpearce@gmail.com>
2016-04-21 02:12:21 +0000

    microposts controller up

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  root 'users#index' # use the `index` action in the `users` controller
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  # get 'blog' => 'microposts#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
